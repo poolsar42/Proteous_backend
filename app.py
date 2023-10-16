0775295d816ebdc753ff888d57714473051bb77c
emil <33105890+poolsar42@users.noreply.github.com>
2023-10-16 18:05:13 +0200

    backend_config

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from flask_cors import CORS, cross_origin
-import os
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -24,5 +23,4 @@ def substring_search():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
