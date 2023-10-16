cca44f57c94e1f2cda3b1bb330bea9b28fd36b5f
emil <33105890+poolsar42@users.noreply.github.com>
2023-10-16 18:26:35 +0200

    from flask to fastapi

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
-from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 df = pd.read_csv('data.csv')  # Assuming data.csv is your CSV file
 
 
-@app.route('/all-entries', methods=['GET'])
-def all_entries():
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+
+@app.get('/all-entries')
+async def all_entries():
     return df.head().to_json(orient='records')
 
 
-@app.route('/substring-search', methods=['GET'])
-def substring_search():
+@app.get('/substring-search')
+async def substring_search():
     substring = request.args.get('substring')
     if substring:
         result = df[df['name'].str.startswith(substring)]
         return result.to_json(orient='records')
     return jsonify([])
-
-
-if __name__ == '__main__':
-    app.run()
